cb9deea99e76944d2cb05b9304b0a8e4c5c0cf6d
KiranSajjan <45354989+KiranSajjan@users.noreply.github.com>
2019-12-16 14:23:01 +0530

    Test project

---
+++ POCProject.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+using POCProject;
+
+namespace POCProject.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DebitMethodTest()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            UnitTesting account = new UnitTesting("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
---
+++ POCProject/UnitTesting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POCProject
+{
+    public class UnitTesting
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        public UnitTesting(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount; // intentionally incorrect code
+        }
+    }
+}
